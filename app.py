972fcd990538cb3a519823c5081e391893f35c92
terumi <terumi@celepar.pr.gov.br>
2021-05-12 08:31:22 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import flask
+from flask import Flask, render_template
+from flask import request
+import predict_qa as predict
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def index():
+    print ("iniciando")
+    #return "<h1>Olá Mundo</h1><p>Teste WS com python</p>"
+    return render_template('index.html')
+
+@app.route("/respostas")
+def respostas():
+    print ("chamou QA")
+    pergunta = [request.args.get('pergunta')]
+    result = predict.predict(pergunta)
+    print ("resposta")
+    print (result)
+    return result
+
+#app.run()
+app.run(host="0.0.0.0", port=int("5000"), debug=False)#map port to 5000
